ee4b336fcfcd6a1631e4a97b32626ab048eb6b6d
robineco <robin.schlund@tutanota.de>
2022-10-14 08:36:28 +0000

    basic logger implementation

---
+++ rubackse-service/lib/logger/logger_service.rb
@@ -0,0 +1,54 @@
+require_relative 'severity'
+
+class Logger
+
+  include Severity
+
+  LOG_LEVEL = %w(DEBUG INFO WARNING ERROR)
+
+  def initialize(level: INFO, source_name: nil)
+    @level = level
+    @source_name = source_name
+  end
+
+  def log(severity, message)
+    if severity >= @level
+      puts format_message(severity, message)
+    end
+  end
+
+  def debug(message)
+    log(DEBUG, message)
+  end
+
+  def info(message)
+    log(INFO, message)
+  end
+
+  def warn(message)
+    log(WARN, message)
+  end
+
+  def error(message)
+    log(ERROR, message)
+  end
+
+  def format_message(severity, message)
+    time = Time.now
+    timestamp = time.strftime("[%d/%m/%Y %H:%M:%S.#{time.usec/1000}]")
+    source = @source_name ? "#{@source_name}: ": ""
+    "#{timestamp} #{format_log_level(severity)} -- #{source}#{message}"
+  end
+
+  def format_log_level(severity)
+    LOG_LEVEL[severity]
+  end
+
+end
+
+log = Logger.new(level: Logger::DEBUG)
+
+log.debug("Hello world")
+log.info("THIS is INFO")
+log.warn("THIS is WARN")
+log.error("THIS is ERROR")
---
+++ rubackse-service/lib/logger/severity.rb
@@ -0,0 +1,8 @@
+class Logger
+   module Severity
+    DEBUG = 0
+    INFO = 1
+    WARN = 2
+    ERROR = 3
+  end
+end
